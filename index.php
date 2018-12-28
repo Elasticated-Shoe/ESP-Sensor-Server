17b1f9796d99b09bd909e00cc5c65b6d3f167a27
Elasticated-Shoe <jmetcalfe24@gmail.com>
2018-12-28 20:38:59 +0000

    Added Different Cases For GET or POST

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 
 $config = parse_ini_file("config.ini"); // settings defined in here
 
-if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) {
-    echo "Using HTTPS";
+// If POST request, check SSL, confirm login details and insert if okay
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) {
+        echo "POST Using HTTPS";
+    }
+}
+// If GET request, serve the page
+elseif($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo "GET";
+}
+else {
+    return 405; // HTTP code for method not allowed
 }
 
 ?>
