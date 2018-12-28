9bdbe973a32ff896b4c7f3c2d0db3662e7a08a70
Elasticated-Shoe <jmetcalfe24@gmail.com>
2018-12-28 20:30:57 +0000

    Added Check For SSL

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
 $config = parse_ini_file("config.ini"); // settings defined in here
 
+if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) {
+    echo "Using HTTPS";
+}
+
 ?>
