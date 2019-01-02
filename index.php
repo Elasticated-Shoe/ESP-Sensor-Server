5f3c85f843a29756a212ce74a6c2c06309bb9ee4
Metcalfe, Jacob <jmetcalfe3@csc.com>
2019-01-02 10:43:15 +0000

    Added Basic Login, Compares Hash With Peppered User Password

--- index.php
+++ index.php
@@ -27,23 +27,42 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $postedPass = $_POST['password'];
         if(gettype($postedPass) === "string" && strlen($postedPass) < 51) {
             $pepperedPostedPass = $config["pepper"] . $postedPass;
-            password_hash($pepperedPostedPass, PASSWORD_BCRYPT, ["cost" => $config["approximateHashTime"]]);
+            // password_hash($pepperedPostedPass, PASSWORD_BCRYPT, ["cost" => $config["approximateHashTime"]]);
             // Create connection
-            $conn = new mysqli($config["servername"], config["username"], config["password"], config["usersDatabase"]);
+            $conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["usersDatabase"]);
             // Check connection
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-
             // read password from database
+            // https://stackoverflow.com/questions/24716560/do-i-need-to-escape-my-variables-if-i-use-mysqli-prepared-statements
+            $sql = $conn->prepare("SELECT passwordHash FROM login.users WHERE username = ?"); // prepared statements / parameterized queries are sufficient to prevent 1st order injection on that statement. If you use un-checked dynamic sql anywhere else in your application you are still vulnerable to 2nd order injection.
+            $sql->bind_param("s", $theUser);
 
-
-
-
-            if(password_verify($pepperedPostedPass, $hashed_password)) {
-                
+            $theUser = $_POST['user'];
+            $result = $sql->execute();
+            if (!$sql->errno) {
+                // Handle error here
+            }
+            $sql->bind_result($passwordHash);
+            // map hashes in results to array
+            $userHashes = array();
+            while ($sql->fetch()) {
+                $userHashes[] = $passwordHash;
+            }
+            // check size of array, as usernames are unique there should only be one, otherwise their is a problem
+            if(sizeof($userHashes) === 1) {
+                if(password_verify($pepperedPostedPass, $userHashes[0])) {
+                    echo "\nCorrect Password!\n";
+                }
+                else {
+                    // need store failed attempts
+                    http_response_code(403); // Return forbidden as password incorrect
+                    die();
+                }
             }
             else {
+                // user not found / count incorrect
                 http_response_code(403); // Return forbidden as password incorrect
                 die();
             }
@@ -54,6 +73,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
     else {
+        // Should block this with apache and seperate out this file into seperate scripts?
         http_response_code(404);
         die();
     }
