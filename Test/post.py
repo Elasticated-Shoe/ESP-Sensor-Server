0b976e20ced6f0a2c08fdb9b843f1ae712143d4b
Metcalfe, Jacob <jmetcalfe3@csc.com>
2019-01-02 10:43:31 +0000

    Test Script For Testing Password Posting

---
+++ Test/post.py
@@ -0,0 +1,25 @@
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+r = requests.post("http://localhost/phplogin/index.php", 
+                    data={'password': '2Jm466!6', 'user': 'jacob'}, 
+                    verify=False) # 404 as no ssl
+print(r)
+print("Retrying..")
+r = requests.post("https://localhost/phplogin/index.php", 
+                    data={'password': '2Jm466!6', 'user': 'bob'}, 
+                    verify=False) # 403 as user not found
+print(r)
+print("Retrying..")
+r = requests.post("https://localhost/phplogin/index.php", 
+                    data={'password': 'nvlalskn38', 'user': 'jacob'}, 
+                    verify=False) # 403 as password incorrect
+print(r)
+print("Retrying..")
+r = requests.post("https://localhost/phplogin/index.php", 
+                    data={'password': '2Jm466!6', 'user': 'jacob'}, 
+                    verify=False) # 200
+print(r)
+print("Complete")
