ec5a8e4af6353d99db4ffab1dd84a0e0dbd1f7c7
Metcalfe, Jacob <jmetcalfe3@csc.com>
2019-01-02 11:22:17 +0000

    Post Now Prints Messages Properly

--- Test/post.py
+++ Test/post.py
@@ -7,19 +7,23 @@ r = requests.post("http://localhost/phplogin/index.php",
                     data={'password': '2Jm466!6', 'user': 'jacob'}, 
                     verify=False) # 404 as no ssl
 print(r)
+print(r.text)
 print("Retrying..")
 r = requests.post("https://localhost/phplogin/index.php", 
                     data={'password': '2Jm466!6', 'user': 'bob'}, 
                     verify=False) # 403 as user not found
 print(r)
+print(r.text)
 print("Retrying..")
 r = requests.post("https://localhost/phplogin/index.php", 
                     data={'password': 'nvlalskn38', 'user': 'jacob'}, 
                     verify=False) # 403 as password incorrect
 print(r)
+print(r.text)
 print("Retrying..")
 r = requests.post("https://localhost/phplogin/index.php", 
                     data={'password': '2Jm466!6', 'user': 'jacob'}, 
                     verify=False) # 200
 print(r)
+print(r.text)
 print("Complete")
