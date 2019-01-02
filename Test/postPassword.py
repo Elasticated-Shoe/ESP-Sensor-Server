103a900f4110cb537233411c82bb8f6f71b41074
Metcalfe, Jacob <jmetcalfe3@csc.com>
2019-01-02 16:08:05 +0000

    Added Test For Posting Data, Moved Things To Config From Code

--- Test/postPassword.py
+++ Test/postPassword.py
@@ -1,28 +1,34 @@
 import requests
 import urllib3
+import configparser
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+config = configparser.ConfigParser() # Initialise the config parser
+config.read("../framework/config.ini") # Read the config file
+config = config["settings"]
+password = config["applicationPassword"].replace('"', "")
+
 r = requests.post("http://localhost/phplogin/index.php", 
-                    data={'password': '2Jm466!6', 'user': 'jacob'}, 
+                    data={'password': "b6b6yv5", 'user': 'jacob'}, 
                     verify=False) # 404 as no ssl
 print(r)
 print(r.text)
 print("Retrying..")
 r = requests.post("https://localhost/phplogin/index.php", 
-                    data={'password': '2Jm466!6', 'user': 'bob'}, 
+                    data={'password': "bytbs", 'user': 'bob'}, 
                     verify=False) # 403 as user not found
 print(r)
 print(r.text)
 print("Retrying..")
 r = requests.post("https://localhost/phplogin/index.php", 
-                    data={'password': 'nvlalskn38', 'user': 'jacob'}, 
+                    data={'password': 'h6h45f43f', 'user': 'jacob'}, 
                     verify=False) # 403 as password incorrect
 print(r)
 print(r.text)
 print("Retrying..")
 r = requests.post("https://localhost/phplogin/index.php", 
-                    data={'password': '2Jm466!6', 'user': 'jacob'}, 
+                    data={'password': password, 'user': 'jacob'}, 
                     verify=False) # 200
 print(r)
 print(r.text)
@@ -30,7 +36,7 @@ print("Trying Incorrect Password Case")
 for i in range(7):
     print("LOOP START")
     r = requests.post("https://localhost/phplogin/index.php", 
-                    data={'password': 'nvlalskn38', 'user': 'jacob'}, 
+                    data={'password': 'ff43j6j', 'user': 'jacob'}, 
                     verify=False) # 403 as password incorrect
     print(r)
     print(r.text)
---
+++ Test/postSensorData.py
@@ -0,0 +1,50 @@
+import requests
+import urllib3
+import time
+from random import randint
+import configparser
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+config = configparser.ConfigParser() # Initialise the config parser
+config.read("../framework/config.ini") # Read the config file
+config = config["settings"]
+password = config["applicationPassword"].replace('"', "")
+
+def calcVal(initial, name, current = None):
+    global lastValues
+    if initial == None:
+        return None
+    if current == None:
+        current = initial
+    while True:
+        increment = randint(-1, 1)
+        if current + increment < 10 or current + increment > 34:
+            continue
+        break
+    current = current + randint(-1, 1)
+    lastValues[name] = current
+    return current
+
+lastValues = {}
+lastValues["Pi"] = None
+lastValues["Omicron"] = None
+lastValues["Epsilon"] = None
+lastValues["Lambda"] = None
+test = None
+
+for i in range(60):
+    if i == 34:
+        test = 25
+    data = {
+        "Pi": calcVal(23, "Pi", lastValues["Pi"]),
+        "Omicron": calcVal(30, "Omicron", lastValues["Omicron"]),
+        "Epsilon": calcVal(15, "Epsilon", lastValues["Epsilon"]),
+        "Lambda": calcVal(test, "Lambda", lastValues["Lambda"])
+    }
+    print(data)
+    r = requests.post("https://localhost/phplogin/index.php", 
+                        data={'password': password, 'user': 'jacob', "data": data}, 
+                        verify=False) # 200
+    print(r.text)
+    time.sleep(1)
