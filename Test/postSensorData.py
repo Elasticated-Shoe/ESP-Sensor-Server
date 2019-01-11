8132962d22b69e31e692da32f45cea1b5c42cc50
jmetcalfe3 <jmetcalfe3@csc.com>
2019-01-11 13:03:24 +0000

    More Types To Test Case

--- Test/postSensorData.py
+++ Test/postSensorData.py
@@ -30,7 +30,16 @@ def calcVal(initial, name, current = None):
 lastValues = {}
 lastValues["Pi"] = None
 lastValues["Omicron"] = None
+lastValues["Alpha"] = None
+lastValues["Beta"] = None
+lastValues["Gamma"] = None
+lastValues["Delta"] = None
 lastValues["Epsilon"] = None
+lastValues["Zeta"] = None
+lastValues["Eta"] = None
+lastValues["Theta"] = None
+lastValues["Iota"] = None
+lastValues["Kappa"] = None
 lastValues["Lambda"] = None
 test = None
 
@@ -38,7 +47,15 @@ for i in range(60):
     if i == 34:
         test = 25
     data = {
-        "Pi": {"value": calcVal(23, "Pi", lastValues["Pi"]), "time": int(time.time()) },
+        "Alpha": {"value": calcVal(26, "Pi", lastValues["Alpha"]), "time": int(time.time()) },
+        "Beta": {"value": calcVal(24, "Pi", lastValues["Beta"]), "time": int(time.time()) },
+        "Gamma": {"value": calcVal(23, "Pi", lastValues["Gamma"]), "time": int(time.time()) },
+        "Delta": {"value": calcVal(23, "Pi", lastValues["Delta"]), "time": int(time.time()) },
+        "Zeta": {"value": calcVal(22, "Pi", lastValues["Zeta"]), "time": int(time.time()) },
+        "Eta": {"value": calcVal(18, "Pi", lastValues["Eta"]), "time": int(time.time()) },
+        "Theta": {"value": calcVal(30, "Pi", lastValues["Theta"]), "time": int(time.time()) },
+        "Iota": {"value": calcVal(19, "Pi", lastValues["Iota"]), "time": int(time.time()) },
+        "Kappa": {"value": calcVal(23, "Pi", lastValues["Kappa"]), "time": int(time.time()) },
         "Omicron": {"value": calcVal(29, "Omicron", lastValues["Omicron"]), "time": int(time.time()) },
         "Epsilon": {"value": calcVal(19, "Epsilon", lastValues["Epsilon"]), "time": int(time.time()) },
         "Lambda": {"value": calcVal(test, "Lambda", lastValues["Lambda"]), "time": int(time.time()) },
