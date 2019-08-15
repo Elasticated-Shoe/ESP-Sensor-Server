e8daab86aea94aa2f2c0bdafd4b5de624b3fb679
Elasticated-Shoe <jmetcalfe24@gmail.com>
2019-08-15 20:30:34 +0100

    API For User Creation

--- public_html/protected/API Actions/Login/CreateUser.php
+++ public_html/protected/API Actions/Login/CreateUser.php
@@ -2,18 +2,21 @@
     require_once("protected/API Actions/Login/LoginBase.php");
 
     class CreateUser extends LoginBase {
-        public $permission = "Create";
+        public $permission = "createUser";
 
         function init() {
+            if(! $this->isValid($_POST["password"]) ) {
+                $this->error = "Invalid Password Format";
+                return;
+            }
+            $pepperedPassword = $this->pepperPassword($_POST["password"]);
+            $hashedPepperedPassword = $this->generateHash($pepperedPassword);
+            // add user to the users table
             $this->callInbuiltQuery(
-                "INSERT INTO users(
-                    username, userPass, userEmail, isLocked
-                ) 
-                VALUES (
-                    ?, ?, ?, ?
-                )",
+                "INSERT INTO users( username, userPass, userEmail, isLocked ) 
+                VALUES ( ?, ?, ?, ? )",
                 null,
-                array("sssi", $_POST["name"], $_POST["password"], $_POST["email"], 0)
+                array("sssi", $_POST["name"], $hashedPepperedPassword, $_POST["email"], 0)
             );
             $this->data = "User Created";  
         }
