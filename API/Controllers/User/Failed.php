9cc6cd9295a52682381b80a132ef9249c62e8ad1
Elasticated-Shoe <JMetcalfe@unifybusiness.co.uk>
2020-04-05 22:15:53 +0100

    Failed Logins Now Works With New Layout

--- API/Controllers/User/Failed.php
+++ API/Controllers/User/Failed.php
@@ -3,13 +3,20 @@
 
     class readFailedLogins extends AbstractController {
         function CheckInput() {
+            if($_SERVER['REQUEST_METHOD'] !== 'GET') {
+                return "Method Must Be GET";
+            }
             return true;
         }
         function CheckPermission() {
+            $DoesLoggedInUserMatchRequestedUser = $this->session->isUser();
+            if(!$DoesLoggedInUserMatchRequestedUser) {
+                return "You Are Not Logged In For That User";
+            }
             return true;
         }
         function Action() {
-            return $this->dbCache->readAttempts($_GET["user"]);
+            return $this->dbCache->readAttempts($this->session->getLogin());
         }
     }
 ?>
