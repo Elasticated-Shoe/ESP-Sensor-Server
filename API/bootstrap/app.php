9e40783b38d280018b9326eeae0b468b4139ad9e
Elasticated-Shoe <jmetcalfe24@gmail.com>
2020-08-15 12:31:45 +0100

    TestRequest FormRequest

--- API/bootstrap/app.php
+++ API/bootstrap/app.php
@@ -96,6 +96,7 @@ $app->routeMiddleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Urameshibr\Providers\FormRequestServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
