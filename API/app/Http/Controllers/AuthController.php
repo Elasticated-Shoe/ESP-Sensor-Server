555071e8142676be48a5ccd41f66bed9345c91c0
Elasticated-Shoe <jmetcalfe24@gmail.com>
2020-12-23 22:32:43 +0000

    Long Life Token Option, owned Claim

--- API/app/Http/Controllers/AuthController.php
+++ API/app/Http/Controllers/AuthController.php
@@ -10,34 +10,39 @@ use App\sensorMetaData;
 class AuthController extends Controller {
     public function getToken(Request $request) {
         $validatedData = $this->validate($request, [
-            'userEmail'     => 'required',
-            'userPass'  => 'required'
+            'userEmail' => 'required',
+            'userPass'  => 'required',
+            'app' => 'bool'
         ]);
 
         $user = users::where("userEmail", $validatedData["userEmail"] )->first();
 
         if($user === null) {
-            return array(
-                "Message" => "Sensor {$id} Not Found"
-            );
-        }
-
-        if( password_verify($validatedData["userPass"], $user["userPass"] ) ) {
-            $payload = [
-                'iss' => "esp-jwt",
-                'sub' => $user["userId"],
-                'iat' => time(),
-                'exp' => time() + 60*60
-            ];
-
             return response()->json([
-                'token' => JWT::encode($payload, env('JWT_SECRET'))
-            ], 200);
+                'error' => 'Username does not exist'
+            ], 404);
         }
+
+        if( !password_verify($validatedData["userPass"], $user["userPass"] ) ) {
+            return response()->json([
+                'error' => 'Email or password is wrong.'
+            ], 401);
+        }
+
+        $ownedSensors = sensorMetadata::where("sensorOwner", $user["userId"])->get()->toArray();
+        $ownedSensorIdArray = array_column($ownedSensors, 'sensorId');
+
+        $payload = [
+            'iss' => "esp-jwt",
+            'sub' => $user["userId"],
+            'iat' => time(),
+            'exp' => $validatedData["app"] ? time() + 60*60*24*365*100 : time() + 60*60,
+            'owned' => $ownedSensorIdArray
+        ];
 
         return response()->json([
-            'error' => 'Email or password is wrong.'
-        ], 400);
+            'token' => JWT::encode($payload, env('JWT_SECRET'))
+        ], 200);
     }
     public function testToken(Request $request) {
         return array(
